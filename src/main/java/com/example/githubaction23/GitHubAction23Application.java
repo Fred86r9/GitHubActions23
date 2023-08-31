63faf64971c2a20d776e9ae03d7d4cf85ffa7f00
Fred86r9 <118350827+Fred86r9@users.noreply.github.com>
2023-08-31 15:05:16 +0200

    First commit

---
+++ src/main/java/com/example/githubaction23/GitHubAction23Application.java
@@ -0,0 +1,13 @@
+package com.example.githubaction23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubAction23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubAction23Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubaction23/GitHubAction23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubaction23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubAction23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
